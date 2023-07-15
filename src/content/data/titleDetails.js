a133d7a8d8f6c51d61a13735bafd3d66797931fe
josh-beck <joshuathomasbeck@gmail.com>
2023-07-15 15:21:38 -0400

    desc changes

--- src/content/data/titleDetails.js
+++ src/content/data/titleDetails.js
@@ -3,4 +3,4 @@ export const NAME = "Tim Plette"
 export const JOB_TITLE = "Website Portfolio Designer";
 
 export const DESC = 
-`Easy to use portfolio website template: Bring your resume, job hunt, or college admission to life!`
+`Easy to use portfolio website template: Bring your job hunt or admission app to life!`
